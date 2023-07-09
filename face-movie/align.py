a2126bdd8a893b7fdddf86b7c6fa801c0b19e5e6
kaho_tam <kaho.tam.ph@gmail.com>
2023-07-09 19:37:30 +0100

    using pathlib; limit img size

--- face-movie/align.py
+++ face-movie/align.py
@@ -5,7 +5,9 @@ import cv2
 import dlib
 import numpy as np
 import argparse
-import os
+from pathlib import Path
+import json
+from PIL import Image
 
 PREDICTOR_PATH = "./shape_predictor_68_face_landmarks.dat"
 
@@ -26,7 +28,11 @@ ALIGN_EYES = ([LEFT_EYE_POINTS[0]] + [RIGHT_EYE_POINTS[0]])
 DETECTOR = dlib.get_frontal_face_detector()
 PREDICTOR = dlib.shape_predictor(PREDICTOR_PATH)
 
+EYE_FILE_NAME = Path("manual_align_coords.json")
+MAX_DIM = (600, 800)
+
 cache = dict()
+global align_coords
 
 def get_landmarks(im, manual=False):
     if (not manual):
@@ -51,14 +57,23 @@ def annotate_landmarks(im, landmarks):
 
 def get_eye_coordinates(impath):
     eye_coordinates = []
-    print(os.path.basename(impath))
-    while len(eye_coordinates) < 2:
-        try:
-            x = int(input("Enter x-coordinate for eye {}: ".format(len(eye_coordinates) + 1)))
-            y = int(input("Enter y-coordinate for eye {}: ".format(len(eye_coordinates) + 1)))
-            eye_coordinates.append((x, y))
-        except ValueError:
-            print(f"Invalid input. Please enter integer values for the coordinates ({os.path.basename(impath)}).")
+    print(impath.name)
+    if (str(impath) in align_coords):
+        eye_coordinates = align_coords[str(impath)]
+    else:
+        while len(eye_coordinates) < 2:
+            try:
+                x = int(input("Enter x-coordinate for eye {}: ".format(len(eye_coordinates) + 1)))
+                y = int(input("Enter y-coordinate for eye {}: ".format(len(eye_coordinates) + 1)))
+                eye_coordinates.append((x, y))
+            except ValueError:
+                print(f"Invalid input. Please enter integer values for the coordinates ({impath.name}).")
+        align_coords[str(impath)] = eye_coordinates
+        with open(EYE_FILE_NAME, 'w', encoding='utf8') as outfile:
+            str_ = json.dumps(align_coords,
+                              indent=4, sort_keys=True,
+                              separators=(',', ': '), ensure_ascii=False)
+            outfile.write(str(str_))
     return np.matrix(eye_coordinates)
     
 def transformation_from_points(points1, points2):
@@ -104,8 +119,9 @@ def transformation_from_points(points1, points2):
 def read_im_and_landmarks(fname, manual=False):
     if fname in cache:
         return cache[fname]
-    im = cv2.imread(fname, cv2.IMREAD_COLOR)
+    im = read_im(fname)
     s = get_landmarks(im, manual)
+    annotate_landmarks(im, s)
 
     cache[fname] = (im, s)
     return im, s
@@ -127,21 +143,31 @@ def warp_im(im, M, dshape, prev):
 
     return output_im
 
+def read_im(impath):
+    im = Image.open(impath)
+    im.thumbnail(MAX_DIM, Image.LANCZOS)
+    return cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
 
 def align_images(impath1, impath2, border, manual, prev=None):
-    filename = os.path.basename(impath2).split('.')[0]
-    outfile = "{}/{}.jpg".format(OUTPUT_DIR, filename)
-    if (not os.path.exists(outfile)):
-        im1, landmarks1 = read_im_and_landmarks(impath1)
-        try:
-            im2, landmarks2 = read_im_and_landmarks(impath2, manual)
-            T = transformation_from_points(landmarks1[ALIGN_POINTS],
-                            landmarks2[ALIGN_POINTS])
-        except Exception as e:
-            eye_coords = get_eye_coordinates(impath2)
-            T = transformation_from_points(landmarks1[ALIGN_EYES],
-                           eye_coords)
-
+    filename = impath2.name
+    outfile = OUTPUT_DIR / filename 
+    if (not outfile.exists()):
+        manual_align = manual
+        if (not manual):
+            try:
+                im1, landmarks1 = read_im_and_landmarks(impath1)
+                im2, landmarks2 = read_im_and_landmarks(impath2)
+                T = transformation_from_points(landmarks1[ALIGN_POINTS],
+                                landmarks2[ALIGN_POINTS])
+            except Exception as e:
+                manual_align = True
+        if (manual_align):
+            eye_coords1 = get_eye_coordinates(impath1)
+            eye_coords2 = get_eye_coordinates(impath2)
+            im2 = Image.open(impath2)
+            im2.thumbnail(MAX_DIM, Image.LANCZOS)
+            im2 = cv2.cvtColor(np.array(im2), cv2.COLOR_RGB2BGR)
+            T = transformation_from_points(eye_coords1, eye_coords2)
         M = cv2.invertAffineTransform(T[:2])
 
         if border is not None:
@@ -150,12 +176,12 @@ def align_images(impath1, impath2, border, manual, prev=None):
 
         warped_im2 = warp_im(im2, M, im1.shape, prev)
 
-        cv2.imwrite(outfile, warped_im2)
+        cv2.imwrite(str(outfile), warped_im2)
         print("Aligned {}".format(filename))
         return warped_im2
     else:
         print("Skipping", outfile)
-        return cv2.imread(outfile, cv2.IMREAD_COLOR)
+        return read_im(outfile)
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
@@ -164,29 +190,37 @@ if __name__ == "__main__":
     ap.add_argument("-overlay", help="Flag to overlay images on top of each other", action='store_true')
     ap.add_argument("-border", type=int, help="Border size (in pixels) to be added to images")
     ap.add_argument("-outdir", help="Output directory name", required=True)
-    ap.add_argument("-manual", help="Manual Rectangle", action='store_true')
+    ap.add_argument("-manual", help="Manual Coordinates", action='store_true')
     args = vars(ap.parse_args())
-    im_dir = args["images"]
-    target = args["target"]
+    im_dir = Path(args["images"])
+    target = Path(args["target"])
     overlay = args["overlay"]
     border = args["border"]
     manual = args["manual"]
-    OUTPUT_DIR = args["outdir"]
+    OUTPUT_DIR = Path(args["outdir"])
 
     valid_formats = [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".tif"]
-    get_ext = lambda f: os.path.splitext(f)[1].lower()
 
     prev = None
+
+    if EYE_FILE_NAME.exists():
+        with open(EYE_FILE_NAME, 'r') as file:
+            align_coords = json.load(file)
+    else:
+        align_coords = dict()
+
+    if not OUTPUT_DIR.exists():
+        OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
     # Constraints on input images (for aligning):
     # - Must have clear frontal view of a face (there may be multiple)
     # - Filenames must be in lexicographic order of the order in which they are to appear  
     
-    im_files = [f for f in os.listdir(im_dir) if get_ext(f) in valid_formats]
-    im_files = sorted(im_files, key=lambda x: x.split('/'))
-    for im in im_files:
+    im_files = [f for f in im_dir.iterdir() if f.suffix in valid_formats]
+    im_files = sorted(im_files, key=lambda x: x.name)
+    for impath in im_files:
         if overlay:
-            prev = align_images(target, im_dir + '/' + im, border, manual, prev)
+            prev = align_images(target, impath, border, manual, prev)
         else:
-            align_images(target, im_dir + '/' + im, border, manual)
+            align_images(target, impath, border, manual)
 
