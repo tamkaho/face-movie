8d15c3f7fccfa1363ab1ff5a1a8128d818faf9f1
tamkaho <kaho.tam.ph@gmail.com>
2025-11-09 22:45:30 -0600

    Merge branch 'master' into windows

--- face-movie/main.py
+++ face-movie/main.py
@@ -672,7 +672,7 @@ def add_text_to_frame(img: np.ndarray, idx: int) -> np.ndarray:
 
     # Load the font
     font_paths = [
-        Path("C:\\Windows\\Fonts\\DejaVuSans.ttf"),
+        Path("C:\\Windows\\Fonts\\Arial.ttf"),
         Path(cv2.__path__[0]) / "qt" / "fonts" / "DejaVuSans.ttf"
         if hasattr(cv2, "__path__")
         else None,
